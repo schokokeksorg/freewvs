5c7da81c803ea110b8e25530f5df3af2e7eeff4e
Hanno Böck <hanno@gentoo.org>
2019-12-12 19:10:28 +0100

    disable jsonlint test on python 3.5 due to different behavior of json.dumps()

--- tests/test_jsonlint.py
+++ tests/test_jsonlint.py
@@ -6,6 +6,9 @@ import difflib
 
 
 class TestJsonLint(unittest.TestCase):
+
+    @unittest.skipIf(sys.version_info < (3, 6, 0),
+                     "json.dumps force-sorts on python 3.5")
     def test_json_lint(self):
         valid = True
         for f in glob.glob("freewvsdb/*.json"):
