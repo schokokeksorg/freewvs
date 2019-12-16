9a78894428fabc39fbcd18a52f40890439f9d34a
Hanno Böck <hanno@gentoo.org>
2019-12-16 20:45:03 +0100

    add sanity checks for json data

--- tests/test_jsonlint.py
+++ tests/test_jsonlint.py
@@ -3,6 +3,7 @@ import glob
 import json
 import sys
 import difflib
+import re
 
 
 class TestJsonLint(unittest.TestCase):
@@ -22,6 +23,29 @@ class TestJsonLint(unittest.TestCase):
                 sys.stdout.writelines(difflib.unified_diff(orig, new))
                 valid = False
         self.assertTrue(valid)
+
+    def test_json_values(self):
+        jconfig = []
+        for cfile in glob.glob('freewvsdb/*.json'):
+            with open(cfile) as json_file:
+                jconfig += json.load(json_file)
+
+        mkeys = set(['name', 'url', 'safe', 'vuln', 'detection'])
+        for item in jconfig:
+
+            # check for all mandatory keys
+            self.assertEqual(mkeys.intersection(item.keys()), mkeys,
+                             msg="Missing key in %s" % item['name'])
+
+            # check we have at least one detection
+            self.assertTrue(len(item['detection']) >= 1,
+                            msg="No detection in %s" % item['name'])
+
+            # vuln needs to be CVE or HTTPS URL
+            self.assertTrue(re.match("^CVE-[0-9]*-[0-9]*$", item['vuln'])
+                            or item['vuln'].startswith("https://"),
+                            msg="%s: Invalid vuln %s" %
+                            (item['name'], item['vuln']))
 
 
 if __name__ == '__main__':
