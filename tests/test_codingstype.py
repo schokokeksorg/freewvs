e3346581481c0921461ca67fbbf99e64af02fa65
Hanno Böck <hanno@gentoo.org>
2019-12-23 18:15:28 +0100

    add update-freewvsdb and setup.py to coding style checks

--- tests/test_codingstype.py
+++ tests/test_codingstype.py
@@ -6,12 +6,14 @@ import glob
 class TestCodingstyle(unittest.TestCase):
     @staticmethod
     def test_codingstyle():
-        pyfiles = ["freewvs"] + glob.glob("tests/*.py")
+        pyfiles = ["freewvs", "update-freewvsdb",
+                   "setup.py"] + glob.glob("tests/*.py")
         subprocess.run(["pycodestyle", "--ignore=W503"]
                        + pyfiles, check=True)
         subprocess.run(["pyflakes"] + pyfiles, check=True)
 
-        pylint_disable = "missing-docstring,invalid-name,too-many-arguments"
+        pylint_disable = "missing-docstring,invalid-name," \
+                         + "too-many-arguments,duplicate-code"
         subprocess.run(["pylint", "--disable=%s" % pylint_disable]
                        + pyfiles, check=True)
 
