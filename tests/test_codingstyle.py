0c677804cfdcd395fb406553245ca2b3aab3967b
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-12-03 21:50:00 +0100

    fix ruff warnings

--- tests/test_codingstyle.py
+++ tests/test_codingstyle.py
@@ -6,21 +6,21 @@ import unittest
 class TestCodingstyle(unittest.TestCase):
     @staticmethod
     def test_codingstyle():
-        pyfiles = ["freewvs", "update-freewvsdb", "setup.py"] + glob.glob("tests/*.py")
+        pyfiles = ["freewvs", "update-freewvsdb", "setup.py", *glob.glob("tests/*.py")]
         subprocess.run(
-            ["pycodestyle", "--ignore=W503,E501", "--max-line-length=88"] + pyfiles,
+            ["pycodestyle", "--ignore=W503,E501", "--max-line-length=88", *pyfiles],
             check=True,
         )
-        subprocess.run(["pyflakes"] + pyfiles, check=True)
+        subprocess.run(["pyflakes", *pyfiles], check=True)
 
         pylint_disable = (
             "missing-docstring,invalid-name,duplicate-code,"
-            + "too-many-arguments,consider-using-with,too-many-positional-arguments"
+            "too-many-arguments,consider-using-with,too-many-positional-arguments"
         )
-        subprocess.run(["pylint", f"--disable={pylint_disable}"] + pyfiles, check=True)
+        subprocess.run(["pylint", f"--disable={pylint_disable}", *pyfiles], check=True)
 
-        subprocess.run(["flake8", "--select=DUO"] + pyfiles, check=True)
-        subprocess.run(["pyupgrade", "--py311-plus"] + pyfiles, check=True)
+        subprocess.run(["flake8", "--select=DUO", *pyfiles], check=True)
+        subprocess.run(["pyupgrade", "--py311-plus", *pyfiles], check=True)
 
 
 if __name__ == "__main__":
